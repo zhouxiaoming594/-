2b5efc067f14c9b6cfd0fef7223ae7f0d9681dee
zhouxiaoming594 <423543593@qq.com>
2016-07-23 15:56:13 +0800

    Add files via upload

---
+++ Untitled-3.js
@@ -0,0 +1,35 @@
+// JavaScript Document
+window.onload=function(){
+	var obtn=document.getElementById('btn');
+	var clientHeight=document.documentElement.clientHeight;
+	var timer=null;
+	var isTop=true;
+	window.onscroll=function(){
+		var osTop=document.documentElement.scrollTop||document.body.scrollTop;
+		if(osTop>=clientHeight){
+			obtn.style.display="block";
+			}
+			else
+			{
+			obtn.style.display="none";
+			}
+		if(!isTop){
+			clearInterval(timer);	
+				   }
+		isTop=false;
+		};
+		
+	obtn.onclick=function(){
+		 timer=setInterval(function(){
+			var osTop=document.documentElement.scrollTop||document.body.scrollTop;
+			var ispeed=Math.floor(-osTop/6);
+			document.documentElement.scrollTop=document.body.scrollTop=osTop+ispeed;
+			isTop=true;
+			if(osTop==0)
+				{
+				clearInterval(timer);
+				}						   
+									   },30);	
+		
+	}	
+}
